cab5d2565c554d582e1f68f0eba778806c86d288
2410vijayakumar <2410vijayakumar@gmail.com>
2026-06-18 13:11:58 +0800

    name change

--- src/main/java/com/example/CI/CD/DEVOPS/controller/HelloController.java
+++ src/main/java/com/example/CI/CD/DEVOPS/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello CI-CD NEELA PROJECT";
+        return "Hello CI-CD NEELA manoj";
     }
 }
