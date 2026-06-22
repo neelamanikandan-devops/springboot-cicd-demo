8a0751264b38ab86693dc10da7e78c199105da8d
2410vijayakumar <2410vijayakumar@gmail.com>
2026-06-22 19:05:04 +0800

    ghi

--- src/main/java/com/example/CI/CD/DEVOPS/controller/HelloController.java
+++ src/main/java/com/example/CI/CD/DEVOPS/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello CI-CD newdata Neela1234";
+        return "Hello CI-CD newdata Neela123";
     }
 }
